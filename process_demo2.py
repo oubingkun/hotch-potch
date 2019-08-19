6174be92d0cf1d22cb356e019cf5dc52c295505b
oubingkun <2878088956@qq.com>
2019-08-19 22:47:18 +0800

    process_demo2 v0.1

--- process_demo2.py
+++ process_demo2.py
@@ -6,12 +6,12 @@ import time
 threadNum = 2
 
 def meizu():
-    udid= ["721CECRE22Y9A","HKP3AF68"]
+    udid= ["721CECXXX2Y9A","HKP3XXX8"]
     for i in udid:
         d = u2.connect(i)
-        d.app_start("com.czur.scanpro")
+        d.app_start("com.xxx.xxx")
         time.sleep(2)
-        d.app_stop('com.czur.scanpro')
+        d.app_stop('com.xxx.xxx')
 
     
 
